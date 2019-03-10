5e7b3161901c94203e004a907f89d58b0f32c3d2
Laila Atrmouh <laila.atrmouh@gmail.com>
2019-03-10 19:21:57 +0100

    Updates

---
+++ precache-manifest.6e74fcf215fa3c86f32193a0f2a430c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5df599429eb1cb7d3f7",
+    "url": "/blindtest/static/css/main.ac4d4c56.chunk.css"
+  },
+  {
+    "revision": "c5df599429eb1cb7d3f7",
+    "url": "/blindtest/static/js/main.61251850.chunk.js"
+  },
+  {
+    "revision": "a3eb1f9170bed9d8242a",
+    "url": "/blindtest/static/js/runtime~main.10c03774.js"
+  },
+  {
+    "revision": "1fc051e5fcbbf4d3855f",
+    "url": "/blindtest/static/js/2.9f54022d.chunk.js"
+  },
+  {
+    "revision": "7c1cff284ad4b6ec3359ebdd208f4de8",
+    "url": "/blindtest/index.html"
+  }
+];
